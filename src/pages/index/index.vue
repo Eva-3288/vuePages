11e542bbf28d6b71b59a9fa46bcb8d6bb6632dc5
xzliu <1030957440@qq.com>
2018-06-28 16:46:18 +0800

    vue 多页面配置

---
+++ src/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img src="./../../assets/logo.png">
+    <a href="./../login.html">跳转到登录页</a>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
